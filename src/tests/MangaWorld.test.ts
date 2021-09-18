b6d48fa5ea4f909af48c271b676a92765a43deb7
Simone Fontanella <fonta9879@gmail.com>
2021-09-18 16:22:53 +0200

    add test case

--- src/tests/MangaWorld.test.ts
+++ src/tests/MangaWorld.test.ts
@@ -1,5 +1,5 @@
 import cheerio from "cheerio";
-import { APIWrapper, SearchRequest } from "paperback-extensions-common";
+import { APIWrapper, MangaStatus, SearchRequest } from "paperback-extensions-common";
 import { MangaWorld } from "../MangaWorld/MangaWorld";
 let wrapper: APIWrapper;
 let source: MangaWorld;
@@ -9,25 +9,31 @@ beforeEach(() => {
   source = new MangaWorld(cheerio);
 });
 
-/*  test("getMangaDetails", async () => {
-  let result = await wrapper.getMangaDetails(source, "1241/chrono-crusade");
+test("getMangaDetails", async () => {
+  let result = await wrapper.getMangaDetails(source, "2173/asadora");
   expect(result.titles[0] == "Asadora");
-}); */
-
-/* test("getChapters", async () => {
-  let result = await wrapper.getChapters(source,"1794");
+  expect(result.author == "URASAWA Naoki");
+  expect(result.status == MangaStatus.ONGOING);
 });
- */
 
-/* test("getChapterDetails", async () => {
+test("getChapters", async () => {
+  let result = await wrapper.getChapters(source,"2278/berserk");
+  expect(result.pop()?.name == "Capitolo 00.01");
+  //SO SAD
+});
+
+
+test("getChapterDetails", async () => {
   let result = await wrapper.getChapterDetails(source, "1684/tokyo-revengers", "60c156351c8dbb7cee36bdaf");
-}); */
-
- test("getSearchResult", async () => {
-  let query = <SearchRequest>{title : "tok"};
-  let result = await wrapper.searchRequest(source,query,1);
+  expect(result.id == "60c156351c8dbb7cee36bdaf");
 });
 
-/* test("getHomePage", async () => {
-   const result = await wrapper.getHomePageSections(source);
-}); */
+test("getSearchResult", async () => {
+  let query = <SearchRequest>{title : "tokyo r"};
+  let result = await wrapper.searchRequest(source,query,1);
+  expect(result.results.pop()?.id == "1684/tokyo-revengers");
+});
+
+test("getHomePage", async () => {
+   let result = await wrapper.getHomePageSections(source);
+});
